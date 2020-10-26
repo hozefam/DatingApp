6c9c5526dafb03a23620fb5eb806312d2aefb1fd
Hozefa <hozefam@gmail.com>
2020-10-27 01:56:19 +0300

    Build Client

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,9 +25,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => {
+            services.AddDbContext<DataContext>(options =>
+            {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
             services.AddControllers();
 
         }
@@ -43,6 +45,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
